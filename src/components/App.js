b078ca74b50951732e6be4a427d1cbb9b4e5b342
Fiona Mellink <fimellink@gmail.com>
2024-02-26 09:16:03 +1100

    Complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,31 @@
 // create your App component here
+import React, { useEffect, useState } from "react";
+
+function App () {
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [image, setImage] = useState("")
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then((r) => r.json())
+        .then(data => {
+            setImage(data.message);
+            setIsLoaded(true);
+      });
+    }, []);
+
+    //   if (!image) {
+    //     return <p>Loading...</p>
+    //   }
+    if (!isLoaded) return <p>Loading...</p>;
+
+    return (
+        <>
+            <h1>Dog CEO</h1>
+            <img src={image} alt="A Random Dog"/>
+        </>
+        
+    )
+}
+
+export default App;
